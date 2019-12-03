2f97c81b86b1a392329f5874d11ec8778de9c0ca
sterhline <sterhline@gmail.com>
2019-12-03 17:21:42 +0200

    added section product-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,19 @@
 $(function(){
 
+    $('.rate-star').rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+      });
 
+      $('.product-slider__inner').slick({
+         infinite: true,
+         slidesToShow: 4,
+         slidesToScroll: 4,
+         dots: true,
+         arrows: false
+         
+      });
     var mixer = mixitup('.products__inner-box');
     
 });
