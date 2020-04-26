4c5113dc9783ea5349eb4a989e51d552c802243a
sterhline <sterline@gmail.com>
2020-04-26 17:49:21 +0300

    add adaptive pages: index, new items, product-one

--- app/js/main.js
+++ app/js/main.js
@@ -11,7 +11,32 @@ $(function(){
          slidesToShow: 4,
          slidesToScroll: 4,
          dots: true,
-         arrows: false         
+         arrows: false, 
+         responsive: [
+          {
+            breakpoint: 1900,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            },
+          },
+          {
+            breakpoint: 1441,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1             
+            },
+          },
+          {
+            breakpoint: 801,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1             
+            },
+          }
+        ]        
       });
 
        $(".js-range-slider").ionRangeSlider({
